e65f244c2d8f31f77826b944ca13871199b4dc93
Feng Bin <bfeng@nltechdev.com>
2019-10-17 08:56:59 +0800

    daily build

--- aspnetcoreapp/Controllers/HomeController.cs
+++ aspnetcoreapp/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using aspnetcoreapp.Models;
+using Aspose.BarCode.Generation;
+using Aspose.BarCode;
+using System.Drawing;
 
 namespace aspnetcoreapp.Controllers
 {
@@ -12,6 +15,7 @@ namespace aspnetcoreapp.Controllers
     {
         public IActionResult Index()
         {
+            GenerateBarCode();
             return View();
         }
 
@@ -24,6 +28,22 @@ namespace aspnetcoreapp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void GenerateBarCode(int number = 1234567)
+        {
+            //  // The path to the documents directory.
+            // string dataDir = ".\\";
+            // // Instantiate barcode object and set differnt barcode properties
+            // BarcodeGenerator  generator = new BarcodeGenerator (EncodeTypes.Code128, number.ToString());
+            // generator.Parameters.Barcode.XDimension.Millimeters = 1f;
+
+            // // Save the image to your system and set its image format to Jpeg
+            // generator.Save(dataDir + "output.jpg", BarCodeImageFormat.Jpeg);
+
+            BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+            Image img = b.Encode(BarcodeLib.TYPE.CODE128, "038000356216", Color.Black, Color.White, 290, 120);
+            img.Save( "newoutput.jpg");
         }
     }
 }
